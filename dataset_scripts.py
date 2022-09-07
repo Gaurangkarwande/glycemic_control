9f5e6ada728430ba6b2c5daa4991f2485c2fd3f6
Gaurangkarwande <gkarwande18@gmail.com>
2022-09-06 22:56:10 -0400

    add comments

--- dataset_scripts.py
+++ dataset_scripts.py
@@ -54,11 +54,13 @@ INSULIN_COLS = [
 def create_base_dataset(df_cohort: pd.DataFrame) -> pd.DataFrame:
     """Create the base dataset from raw cohort"""
 
+    # patient map : {patient_id : [list of stay ids]}
     stay_patient_map = defaultdict(list)
     df_unique_stays = df_cohort[["stay_id", "subject_id"]].drop_duplicates(subset="stay_id")
     for row in df_unique_stays.itertuples():
         stay_patient_map[row.subject_id].append(row.stay_id)
 
+    # select patients with only one ICU stay
     selected_patient_ids = set()
     for key in stay_patient_map:
         if len(stay_patient_map[key]) == 1:
@@ -66,12 +68,16 @@ def create_base_dataset(df_cohort: pd.DataFrame) -> pd.DataFrame:
 
     df = df_cohort[df_cohort.subject_id.isin(selected_patient_ids)]
 
+    # select stays with duration within interquantile range [6, 27]
     stay_counts_df = df.groupby(["stay_id"])["glucose"].count()
     filtered_stay_count_df = stay_counts_df[(6 <= stay_counts_df) & (stay_counts_df <= 27)]
     stay_id_set = set(filtered_stay_count_df.index)
     filtered_df = df[df.stay_id.isin(stay_id_set)]
+
+    # only keep the columns we are interested in
     raw_covariate_df = filtered_df[COLUMN_SUBSET]
 
+    # derive clinically relevant covariates
     covariate_df = raw_covariate_df.copy()
     covariate_df["tube_feeding"] = raw_covariate_df[TUBE_FEEDING_COLS].mean(axis=1)
     covariate_df["dextrose"] = raw_covariate_df[DEXTROSE_COLS].mean(axis=1)
